a66534081ad080c9b60ae741619a057e73d41b34
Raryson Pereira Rost <raryson.rost@gmail.com>
2018-10-08 09:35:13 -0300

    fixed a typo

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.send('index.html')
 })
 
-app.listen(env.process.PORT || 3000)
+app.listen(process.env.PORT || 3000)
