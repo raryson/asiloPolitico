c3ad70df06353446a1c7ff648dc608df4d1b4250
Raryson Pereira Rost <raryson.rost@gmail.com>
2018-10-09 23:21:50 -0300

    removed redirect to ssl

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  if (req.headers['x-forwarded-proto'] != 'https') {        
-    res.redirect("https://" + req.headers.host + req.url)
-  } else {
-    next()
-  }
   res.send('index.html')
 })
 
