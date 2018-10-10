9a70047501b0e7e76e91fb103343e0c101ccb19b
Raryson Pereira Rost <raryson.rost@gmail.com>
2018-10-09 23:20:02 -0300

    removed force ssh

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {        
+  if (req.headers['x-forwarded-proto'] != 'https') {        
     res.redirect("https://" + req.headers.host + req.url)
   } else {
     next()
